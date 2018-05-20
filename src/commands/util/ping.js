1bc6ab1dbcadbc12c72aa5cf3c9e11fc0ba5096b
Serhann <serhanpw@gmail.com>
2018-05-20 23:14:19 +0300

    fix

--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -19,22 +19,20 @@ module.exports = class PingCommand extends Command {
 	async run(msg) {
 		if(!msg.editable) {
 			const pingMsg = await msg.reply('Hesaplanıyor...');
-			let edit = pingMsg.editedTimestamp - msg.createdTimestamp;
 			const embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
 			.setDescription(oneLine`
-				:ping_pong: Mesaj gecikmesi: ${edit / 100}s.
+				:ping_pong: Mesaj gecikmesi: ${(pingMsg.editedTimestamp - msg.createdTimestamp) / 100}s.
 				${this.client.ping ? `Normal gecikme: ${this.client.ping / 100}s.` : ''}
 			`);
 			
 			return pingMsg.edit(msg.channel.type !== 'dm' ? `${msg.author},` : '', { embed });
 		} else {
 			await msg.edit('Hesaplanıyor...');
-			let edit = msg.editedTimestamp - msg.createdTimestamp;
 			const embed = new Discord.RichEmbed()
 			.setColor('RANDOM')
 			.setDescription(oneLine`
-				:ping_pong: Mesaj gecikmesi: ${edit / 100}s.
+				:ping_pong: Mesaj gecikmesi: ${(msg.editedTimestamp - msg.createdTimestamp) / 100}s.
 				${this.client.ping ? `Normal gecikme: ${this.client.ping / 100}s.` : ''}
 			`);
 			
